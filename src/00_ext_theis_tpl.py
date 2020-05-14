dcb7aacc0dfa990acb6b0ebae9450d898386e765
MuellerSeb <mueller.seb@posteo.de>
2020-05-14 19:04:52 +0200

    add effective-head plots; add convergence plot

---
+++ src/00_ext_theis_tpl.py
@@ -0,0 +1,56 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from anaflow import theis, ext_theis_tpl
+
+plt.style.use('ggplot')
+
+time_ticks = []
+time_labels = ["10 s", "10 min", "10 h"]
+time = [10, 600, 36000]      # 10s, 10min, 10h
+rad = np.geomspace(0.05, 4)  # radial distance from the pumping well in [0, 4]
+S = 1e-4                     # storage
+KG = 1e-4                    # the geometric mean of the conductivity
+len_scale = 20.0             # upper bound for the length scale
+hurst = 0.5                  # hurst coefficient
+var = 0.5                    # variance of the log-conductivity
+rate = -1e-4                 # pumping rate
+KH = KG * np.exp(-var / 2)   # the harmonic mean of the conductivity
+
+head_KG = theis(time, rad, S, KG, rate)
+head_KH = theis(time, rad, S, KH, rate)
+head_ef = ext_theis_tpl(
+    time=time,
+    rad=rad,
+    storage=S,
+    cond_gmean=KG,
+    len_scale=len_scale,
+    hurst=hurst,
+    var=var,
+    rate=rate,
+)
+for i, step in enumerate(time):
+    label_TG = "Theis($K_G$)" if i == 0 else None
+    label_TH = "Theis($K_H$)" if i == 0 else None
+    label_ef = "extended Theis TPL 2D" if i == 0 else None
+    color = "C" + str(i + 1)
+    plt.plot(rad, head_KG[i], label=label_TG, color=color, linestyle="--")
+    plt.plot(rad, head_KH[i], label=label_TH, color=color, linestyle=":")
+    plt.plot(rad, head_ef[i], label=label_ef, color=color)
+    text_v = head_ef[i][-1]
+    plt.annotate(
+        time_labels[i],
+        xy=(rad[-1], text_v),
+        xytext=(rad[-1] + 0.1, text_v),
+        verticalalignment="center",
+    )
+
+# plt.xscale("log")
+plt.xlabel("r in [m]")
+plt.ylabel("h in [m]")
+plt.legend()
+ylim = plt.gca().get_ylim()
+plt.gca().set_xlim([rad[0], rad[-1]])
+plt.tight_layout()
+plt.savefig(os.path.join("..", "results", "00_ext_theis_tpl_2D.png"), dpi=150)
+plt.show()
---
+++ src/01_convergence.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from anaflow import ext_theis_tpl, ext_thiem_tpl
+
+plt.style.use('ggplot')
+
+time = 1e4                   # time point for steady state
+rad = np.geomspace(0.1, 10)  # radius from the pumping well in [0, 4]
+r_ref = 10.0                 # reference radius
+KG = 1e-4                    # the geometric mean of the transmissivity
+len_scale = 5.0              # correlation length of the log-transmissivity
+hurst = 0.5                  # hurst coefficient
+var = 0.5                    # variance of the log-transmissivity
+rate = -1e-4                 # pumping rate
+dim = 1.5                    # using a fractional dimension
+
+head1 = ext_thiem_tpl(rad, r_ref, KG, len_scale, hurst, var, dim=dim, rate=rate)
+head2 = ext_theis_tpl(time, rad, 1e-4, KG, len_scale, hurst, var, dim=dim, rate=rate, r_bound=r_ref)
+head3 = ext_theis_tpl(time, rad, 1e-4, KG, len_scale, hurst, var, dim=dim, rate=rate)
+head3 -= head3[-1]  # quasi-steady
+
+plt.plot(rad, head1, label="Ext Thiem TPL (steady state)")
+plt.plot(rad, head2, label="Ext Theis TPL bounded (t={})".format(time), linestyle="--")
+plt.plot(rad, head3, label="Ext Theis TPL quasi-steady (t={})".format(time), linestyle=":")
+
+plt.xlabel("r in [m]")
+plt.ylabel("h in [m]")
+plt.legend()
+plt.tight_layout()
+plt.savefig(os.path.join("..", "results", "01_ext_theis_tpl_conv.png"), dpi=150)
+plt.show()
