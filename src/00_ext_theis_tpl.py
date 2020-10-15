061ff4511e1a7c5e062df5e9d78e0f071324574e
MuellerSeb <mueller.seb@posteo.de>
2020-10-15 17:26:39 +0200

    src: update tpl and convergence plots to unique style

--- src/00_ext_theis_tpl.py
+++ src/00_ext_theis_tpl.py
@@ -1,14 +1,20 @@
 import os
 import numpy as np
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 from anaflow import theis, ext_theis_tpl
 
-plt.style.use('ggplot')
+plt.style.use('default')
+mpl.rc("text", usetex=True)
+mpl.rc('lines', linewidth=2.5)
+plt.close("all")
+fig = plt.figure(figsize=[5, 3])
+ax = fig.add_subplot(1, 1, 1)
 
 time_ticks = []
-time_labels = ["10 s", "10 min", "10 h"]
+time_labels = [r"$t=10$s", r"$t=10$min", r"$t=10$h"]
 time = [10, 600, 36000]      # 10s, 10min, 10h
-rad = np.geomspace(0.05, 4)  # radial distance from the pumping well in [0, 4]
+rad = np.geomspace(0.05, 4.2)  # radial distance from the pumping well in [0, 4]
 S = 1e-4                     # storage
 KG = 1e-4                    # the geometric mean of the conductivity
 len_scale = 20.0             # upper bound for the length scale
@@ -32,25 +38,27 @@ head_ef = ext_theis_tpl(
 for i, step in enumerate(time):
     label_TG = "Theis($K_G$)" if i == 0 else None
     label_TH = "Theis($K_H$)" if i == 0 else None
-    label_ef = "extended Theis TPL 2D" if i == 0 else None
-    color = "C" + str(i + 1)
-    plt.plot(rad, head_KG[i], label=label_TG, color=color, linestyle="--")
-    plt.plot(rad, head_KH[i], label=label_TH, color=color, linestyle=":")
-    plt.plot(rad, head_ef[i], label=label_ef, color=color)
+    label_ef = "Ext. Theis TPL" if i == 0 else None
+    color = "C0"  # + str(i + 1)
+    ax.plot(rad, head_KG[i], label=label_TG, color=color, linestyle="--", alpha=0.7)
+    ax.plot(rad, head_KH[i], label=label_TH, color=color, linestyle=":", alpha=0.7)
+    ax.plot(rad, head_ef[i], label=label_ef, color=color)
     text_v = head_ef[i][-1]
-    plt.annotate(
+    ax.annotate(
         time_labels[i],
         xy=(rad[-1], text_v),
         xytext=(rad[-1] + 0.1, text_v),
         verticalalignment="center",
     )
-
-# plt.xscale("log")
-plt.xlabel("r in [m]")
-plt.ylabel("h in [m]")
-plt.legend()
-ylim = plt.gca().get_ylim()
-plt.gca().set_xlim([rad[0], rad[-1]])
-plt.tight_layout()
-plt.savefig(os.path.join("..", "results", "00_ext_theis_tpl_2D.png"), dpi=150)
-plt.show()
+ax.set_xlim([-0.2, 4.2])
+ax.set_xticks([0, 1, 2, 3, 4])
+ax.set_ylim([-1.7, 0.1])
+ax.set_yticks([-1.5, -1, -0.5, 0])
+ax.set_xlabel("$r$ in $[m]$")
+ax.set_ylabel("$h(r)$ in $[m]$")
+ax.grid(linestyle=':')
+ax.legend()
+ylim = ax.get_ylim()
+fig.tight_layout()
+fig.savefig(os.path.join("..", "results", "00_ext_theis_tpl_2D.pdf"), dpi=300)
+fig.show()
--- src/01_convergence.py
+++ src/01_convergence.py
@@ -4,14 +4,18 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 from anaflow import ext_theis_tpl, ext_thiem_tpl
 
-plt.style.use('ggplot')
-mpl.rc('lines', linewidth=3)
+plt.style.use('default')
+mpl.rc("text", usetex=True)
+mpl.rc('lines', linewidth=3.5)
+plt.close("all")
+fig = plt.figure(figsize=[5, 3])
+ax = fig.add_subplot(1, 1, 1)
 
 time = 1e4                   # time point for steady state
-rad = np.geomspace(0.1, 10)  # radius from the pumping well in [0, 4]
-r_ref = 10.0                 # reference radius
+rad = np.geomspace(0.1, 9)  # radius from the pumping well in [0, 4]
+r_ref = 9.0                 # reference radius
 KG = 1e-4                    # the geometric mean of the transmissivity
-len_scale = 5.0              # correlation length of the log-transmissivity
+len_scale = 3.0              # correlation length of the log-transmissivity
 hurst = 0.5                  # hurst coefficient
 var = 0.5                    # variance of the log-transmissivity
 rate = -1e-4                 # pumping rate
@@ -22,13 +26,18 @@ head2 = ext_theis_tpl(time, rad, 1e-4, KG, len_scale, hurst, var, dim=dim, rate=
 head3 = ext_theis_tpl(time, rad, 1e-4, KG, len_scale, hurst, var, dim=dim, rate=rate)
 head3 -= head3[-1]  # quasi-steady
 
-plt.plot(rad, head1, label="Ext Thiem TPL (steady state)")
-plt.plot(rad, head2, label="Ext Theis TPL bounded (t={})".format(time), linestyle="--")
-plt.plot(rad, head3, label="Ext Theis TPL quasi-steady (t={})".format(time), linestyle=":", color="k")
-
-plt.xlabel("r in [m]")
-plt.ylabel("h in [m]")
-plt.legend()
-plt.tight_layout()
-plt.savefig(os.path.join("..", "results", "01_ext_theis_tpl_conv.png"), dpi=150)
-plt.show()
+ax.plot(rad, head1, label="Ext. Thiem TPL (steady state)")
+ax.plot(rad, head2, label="Ext. Theis TPL bounded (t={})".format(time), linestyle="--")
+ax.plot(rad, head3, label="Ext. Theis TPL quasi-steady (t={})".format(time), linestyle=":", color="k")
+ax.set_xticks([0, len_scale, 2 * len_scale, rad[-1]])
+ax.set_xticklabels(["$0$", "$\ell$", "$2\ell$", "$3\ell$"])
+ax.set_ylim([-1.7, 0.2])
+ax.set_xlim([-0.3, 9.3])
+ax.set_yticks([-1.5, -1, -0.5, 0])
+ax.set_xlabel("$r$ in $[m]$")
+ax.set_ylabel("$h(r)$ in $[m]$")
+ax.grid(linestyle=':')
+ax.legend()
+fig.tight_layout()
+fig.savefig(os.path.join("..", "results", "01_ext_theis_tpl_conv.pdf"), dpi=300)
+fig.show()
